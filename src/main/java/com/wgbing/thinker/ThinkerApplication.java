65e50409168f4b94e14370536762082dc399ad29
wgbing <761590577@qq.com>
2018-07-10 16:02:09 +0800

    Initial commit

---
+++ src/main/java/com/wgbing/thinker/ThinkerApplication.java
@@ -0,0 +1,12 @@
+package com.wgbing.thinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wgbing/thinker/web/LoginController.java
@@ -0,0 +1,21 @@
+package com.wgbing.thinker.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * TODO: 系统登录控制层
+ * @author wgbing
+ * @date 2018/7/10 15:35
+ */
+@Controller
+public class LoginController {
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+}
