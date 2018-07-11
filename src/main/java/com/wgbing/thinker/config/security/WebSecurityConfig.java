5083b9465bb0bb8cf01385dcec6a01414df2ef7f
wgbing <761590577@qq.com>
2018-07-11 18:34:20 +0800

    更新

--- src/main/java/com/wgbing/thinker/config/security/WebSecurityConfig.java
+++ src/main/java/com/wgbing/thinker/config/security/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.wgbing.thinker.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 /**
- * TODO: 类描述
+ * TODO: Spring Security 配置
  * @author wgbing
  * @date 2018/7/10 18:00
  */
@@ -14,4 +18,45 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)//开启security注解
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();             //解决SpringBoot不允许加载iframe问题
+        http.exceptionHandling().accessDeniedPage("/denied");//没有权限访问时跳转路径
+        /*.csrf().disable()*/
+        http
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/fonts/**").permitAll()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/libs/**").permitAll()
+                .antMatchers("/plugins/**").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/auth")
+                .failureUrl("/login?error")
+                .and()
+            .logout() //logout config
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/login");
+
+    }
+
+    /**
+     * TODO: 重写父类方法，添加自定义用户及角色
+     * @author wgbing
+     * @date 2018/7/11 9:43
+     * @param auth
+     * @return
+     * @throws
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 }
