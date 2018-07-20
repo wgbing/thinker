9122db24780faf5379e61fdde48a1ee043875fcf
wgbing <761590577@qq.com>
2018-07-20 19:25:22 +0800

    更新

--- src/main/java/com/wgbing/thinker/web/controller/system/OrganizationController.java
+++ src/main/java/com/wgbing/thinker/web/controller/system/OrganizationController.java
@@ -1,4 +1,25 @@
 package com.wgbing.thinker.web.controller.system;
 
+import com.wgbing.thinker.web.controller.LoginController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * TODO: 用户组织机构控制层
+ * @author wgbing
+ * @date 2018/7/20 18:49
+ */
+@Controller
+@RequestMapping("/org")
 public class OrganizationController {
+    private static final Logger log = LoggerFactory.getLogger(OrganizationController.class);
+
+    @GetMapping("/index")
+    public String orgIndex(){
+        return "/system/org/index";
+    }
+
 }
